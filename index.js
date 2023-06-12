726b25e80041abb0ac505cd92530ea295cacd700
Holedesu <88774183+Holedesu@users.noreply.github.com>
2023-06-12 20:12:07 +0300

    Added some api logic

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const x = 5;
-const y = 5;
-console.log(x + y);
-console.log(x - y);
+const apiKey = 'd50fea6da78846e2adb160603231206';
+const apiUrl = 'http://api.weatherapi.com/v1/current.json';
+const city = '&q=London';
+
+async function checkWeather() {
+  const response = await fetch(apiUrl + `?key=${apiKey}` + city);
+  let data = await response.json();
+    console.log(data);
+
+    document.querySelector('.some_class').innerHTML = data.current.feelslike_c + '°C';
+}
+
+checkWeather();
