71991af8f44aca928c93279a87474c777006a00c
Holedesu <88774183+Holedesu@users.noreply.github.com>
2023-06-12 20:17:15 +0300

    fixed linter

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const apiUrl = 'http://api.weatherapi.com/v1/current.json';
 const city = '&q=London';
 
 async function checkWeather() {
-  const response = await fetch(apiUrl + `?key=${apiKey}` + city);
-  let data = await response.json();
-    console.log(data);
+  const response = await fetch(`${apiUrl}?key=${apiKey}${city}`);
+  const data = await response.json();
+  console.log(data);
 
-    document.querySelector('.some_class').innerHTML = data.current.feelslike_c + '°C';
+  document.querySelector('.some_class').innerHTML = `${data.current.feelslike_c}°C`;
 }
 
 checkWeather();
