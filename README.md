97f86102d7851836b3885745412babceb3196e6c
Karina Buga <memnaya@mail.ru>
2023-06-16 16:29:45 +0000

    done 1/2 verstka

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # WeatherAggregator
 [![Node CI](https://github.com/Memnaya/WeatherAggregator/actions/workflows/nodejs.yml/badge.svg)](https://github.com/Memnaya/WeatherAggregator/actions/workflows/nodejs.yml)
+<img src="./img/image_2023-06-16_15-38-34.png" alt="Главная страница">
+<img src="./img/image_2023-06-16_15-39-47.png" alt="Виджеты">
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const apiKey = 'd50fea6da78846e2adb160603231206';
 const apiUrl = 'http://api.weatherapi.com/v1/current.json';
-const city = '&q=London';
+const city = '&q=Saint-Petersburg';
 
 async function checkWeather() {
   const response = await fetch(apiUrl + `?key=${apiKey}` + city);
   let data = await response.json();
     console.log(data);
 
-    document.querySelector('.some_class').innerHTML = data.current.feelslike_c + '°C';
+    document.querySelector('.current_temp').innerHTML = data.current.feelslike_c + '°C';
 }
 
 checkWeather();
+
