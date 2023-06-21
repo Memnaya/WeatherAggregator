31c208c84ba2977f7e767f0564f72c9683834b99
Holedesu <88774183+Holedesu@users.noreply.github.com>
2023-06-21 20:51:05 +0300

    done left layout and js

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const apiKey = 'd50fea6da78846e2adb160603231206';
 const apiUrl = 'http://api.weatherapi.com/v1/current.json';
 const apiUrlAstro = 'http://api.weatherapi.com/v1/astronomy.json';
+const api7Days = 'https://api.weatherapi.com/v1/forecast.json';
 const searchBox = document.querySelector(' .js_input');
-const searchBtn = document.querySelector(' .js_btn');
 
 async function checkWeather(city = 'Saint-Petersburg') {
-  // const responseCurrent = await fetch('http://api.weatherapi.com/v1/current.json?key=d50fea6da78846e2adb160603231206&q=London');
   const responseCurrent = await fetch(`${apiUrl}?key=${apiKey}&q=${city}`);
   const data = await responseCurrent.json();
   const responseAstro = await fetch(`${apiUrlAstro}?key=${apiKey}&q=${city}`);
   const dataAstro = await responseAstro.json();
+  const response7Days = await fetch(`${api7Days}?key=${apiKey}&q=${city}&days=7`);
+  const data7days = await response7Days.json();
+  const weatherCondition = document.querySelector('.weather_cond_main');
   console.log(data);
   console.log(dataAstro);
-
+  console.log(data7days);
   document.querySelector('.current_temp').innerHTML = `${Math.round(data.current.temp_c)}°C`;
   document.querySelector('.pressure_value').innerHTML = `${data.current.pressure_mb}`;
   document.querySelector('.feels_like_value').innerHTML = `${Math.round(data.current.feelslike_c)}°C`;
@@ -33,10 +35,60 @@ async function checkWeather(city = 'Saint-Petersburg') {
       break;
     default:
       console.log();
+      break;
   }
+  document.querySelector('.weather_name').innerHTML = data.current.condition.text;
+  weatherCondition.src = data.current.condition.icon;
+  // console.log(data.current.condition.text);
+  // switch (data.current.condition.text) {
+  //   case 'Partly cloudy' || 'Overcast' ||:
+  //     weatherCondition.src = 'img/today/partly_cloudy.svg';
+  //     document.querySelector('.weather_name').innerHTML = 'Немного облачно';
+  //     break;
+  //   case 'Cloudy':
+  //     weatherCondition.src = 'img/today/cloudy.svg';
+  //     document.querySelector('.weather_name').innerHTML = 'Облачно';
+  //     break;
+  //   case 'Sunny':
+  //     weatherCondition.src = 'img/today/clear_sunny.svg';
+  //     document.querySelector('.weather_name').innerHTML = 'Солнечно';
+  //     break;
+  //   case 'Overcast':
+  //     weatherCondition.src = 'img/today/cloudy.svg';
+  //     document.querySelector('.weather_name').innerHTML = 'Пасмурно';
+  //     break;
+  //   case 'Mist':
+  //     weatherCondition.src = 'img/today/cloudy.svg';
+  //     document.querySelector('.weather_name').innerHTML = 'Туман';
+  //     break;
+  //   case 'Patchy rain possible':
+  //     weatherCondition.src = 'img/today/pour.svg';
+  //     document.querySelector('.weather_name').innerHTML = 'Местами возможен дождь';
+  //     break;
+  //   case 'Patchy rain possible':
+  //     weatherCondition.src = 'img/today/pour.svg';
+  //     document.querySelector('.weather_name').innerHTML = 'Местами возможен дождь';
+  //     break;
+  //   case 'Patchy rain possible':
+  //     weatherCondition.src = 'img/today/pour.svg';
+  //     document.querySelector('.weather_name').innerHTML = 'Местами возможен дождь';
+  //     break;
+  //   case 'Patchy rain possible':
+  //     weatherCondition.src = 'img/today/pour.svg';
+  //     document.querySelector('.weather_name').innerHTML = 'Местами возможен дождь';
+  //     break;
+  //   default:
+  //     console.log();
+  //     break;
+  // }
+
+  let { sunset } = dataAstro.astronomy.astro;
+  let { sunrise } = dataAstro.astronomy.astro;
+  sunset = sunset.slice(0, sunset.length - 3);
+  sunrise = sunrise.slice(0, sunset.length);
   document.querySelector('.rainfall_value').innerHTML = `${data.current.precip_mm}мм`;
-  document.querySelector('.sunset_time').innerHTML = `${dataAstro.astronomy.astro.sunset}`;
-  document.querySelector('.sunrise_time').innerHTML = ` ${dataAstro.astronomy.astro.sunrise}`;
+  document.querySelector('.sunset_time').innerHTML = sunset;
+  document.querySelector('.sunrise_time').innerHTML = ` ${sunrise}`;
 }
 
 // eslint-disable-next-line no-unused-vars
